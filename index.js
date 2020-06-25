c9499a1c1d66e05e4d2b8149f3e3c30111ff0018
Mona Lisa <nikruzhok@gmail.com>
2020-06-26 00:57:21 +0300

    commander

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-const fs = require('fs');
-const path = require('path');
-const {Logr} = require('logr-node-client');
-const {Watcher} = require('./watcher');
+const fs = require('fs')
+const path = require('path')
+const program = require('commander')
+const {Logr} = require('logr-node-client')
+const {Watcher} = require('./watcher')
 
-const configPath = path.resolve(process.cwd(), './config.js')
-const config = require(configPath);
+let {version} = require('./package')
 
-const logr = new Logr({
-    udp: ":7776",
-    publicKey: "MCAwDQYJKoZIhvcNAQEBBQADDwAwDAIFAMg7IrMCAwEAAQ==",
-    privateKey: "MC0CAQACBQDIOyKzAgMBAAECBQCHaZwRAgMA0nkCAwDziwIDAL+xAgJMKwICGq0=",
-});
+program
+    .option('-c, --config <string>', 'config path', null, false)
+    .version(version, '-v, --version')
 
-const watchers = []
-for (const file of config.files) {
-    // console.log(file.path, fs.existsSync(file.path))
-    if (!file.path || !fs.existsSync(file.path)) {
-        continue
-    }
-    const watcher = new Watcher(file, logr)
-    watchers.push(watcher)
-}
+program
+    .command('help')
+    .description('show help message')
+    .action(() => program.help())
+
+program
+    .action(() => {
+        const configPath = path.resolve(process.cwd(), program.config || 'config.js')
+        const config = require(configPath);
+
+        const logr = new Logr({
+            udp: ":7776",
+            publicKey: "MCAwDQYJKoZIhvcNAQEBBQADDwAwDAIFAMg7IrMCAwEAAQ==",
+            privateKey: "MC0CAQACBQDIOyKzAgMBAAECBQCHaZwRAgMA0nkCAwDziwIDAL+xAgJMKwICGq0=",
+        });
+
+        const watchers = []
+        for (const file of config.files) {
+            // console.log(file.path, fs.existsSync(file.path))
+            if (!file.path || !fs.existsSync(file.path)) {
+                continue
+            }
+            const watcher = new Watcher(file, logr)
+            watchers.push(watcher)
+        }
+    })
+
+program.parse(process.argv)
