7465d8d91e102993ac58f39fd91ad6fc2c833e8e
Mona Lisa <nikruzhok@gmail.com>
2020-06-30 00:23:47 +0300

    bin logrw

--- config.template.js
+++ config.template.js
@@ -1,7 +1,7 @@
 module.exports = {
     udp: ':7776',
-    public_key: 'MCAwDQYJKoZIhvcNAQEBBQADDwAwDAIFAMg7IrMCAwEAAQ==',
-    private_key: 'MC0CAQACBQDIOyKzAgMBAAECBQCHaZwRAgMA0nkCAwDziwIDAL+xAgJMKwICGq0=',
+    public_key: '',
+    private_key: '',
     files: [
         {
             path: '/var/log/system.log',
